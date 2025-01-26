6a962dbed8f7ad43ad5ede9750a49b6ef3bfbb32
rohit0407org <rohit.rai@nineti.de>
2025-01-26 18:11:27 +0530

    DONE CHANGES

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,26 +6,9 @@ import 'ble_device_list_screen.dart'; // For handling permissions
 class InitialScreen extends StatelessWidget {
   const InitialScreen({super.key});
 
-  Future<void> _requestPermissions(BuildContext context) async {
-    // Request necessary permissions (e.g., location, Bluetooth)
-    final status = await Permission.location.request();
-    if (status.isGranted) {
-      // Navigate to BLE device list screen
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const BLEDeviceListScreen(),
-        ),
-      );
-    } else {
-      // Show error if permissions are denied
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Permissions are required to proceed.'),
-        ),
-      );
-    }
-  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +18,14 @@ class InitialScreen extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () => _requestPermissions(context),
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const BLEDeviceListScreen(),
+              ),
+            );
+          },
           child: const Text('Start Provisioning'),
         ),
       ),
